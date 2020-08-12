d54db86cb2095a0bf08c42986d1cbe03285ed9dc
GPrince00 <gustavoprince0@gmail.com>
2020-08-11 23:08:22 -0300

    feat(game): creates the base

---
+++ game.js
@@ -0,0 +1,7 @@
+console.log("Prince Flappy Bird");
+
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
