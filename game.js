a341094613f06d127ca59ddac16d14dbc1976a3f
GPrince00 <gustavoprince0@gmail.com>
2020-08-12 23:15:49 -0300

    feat(floor): adds the floor

--- game.js
+++ game.js
@@ -6,6 +6,31 @@ sprites.src = './sprites.png';
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+const floor = {
+    spriteX: 0,
+    spriteY: 610,
+    width: 224,
+    heigth: 112,
+    x: 0,
+    y: canvas.height -112,
+    draw() {
+        ctx.drawImage(
+            sprites,
+            floor.spriteX, floor.spriteY,
+            floor.width, floor.heigth,
+            floor.x, floor.y,
+            floor.width, floor.heigth,
+        );
+        ctx.drawImage(
+            sprites,
+            floor.spriteX, floor.spriteY,
+            floor.width, floor.heigth,
+            (floor.x + floor.width), floor.y,
+            floor.width, floor.heigth,
+        );
+    }
+}
+
 const flappyBird = {
     spriteX: 0,
     spriteY: 0,
@@ -25,6 +50,7 @@ const flappyBird = {
 }
 
 function loop() {
+    floor.draw();
     flappyBird.draw();
     
     requestAnimationFrame(loop);
