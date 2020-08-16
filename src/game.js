a56b62b0ef074ce888ee38355d7afd71b9b0c6b0
GPrince00 <gustavoprince0@gmail.com>
2020-08-15 22:41:58 -0300

    feat(app): separates the files in folders

--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
 console.log("Prince Flappy Bird");
 
 const sprites = new Image();
-sprites.src = './sprites.png';
+sprites.src = '../assets/sprites.png';
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
